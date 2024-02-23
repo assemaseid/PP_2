8401c53fdaf1d18edd362b583908a943ab234110
assemaseid <seidkarim601705@gmail.com>
2024-02-23 23:41:25 +0600

    Math, Date, Generator

---
+++ Practice/lab 4/data.py
@@ -0,0 +1,33 @@
+import datetime
+
+#ex 1
+print("Current date: ", datetime.datetime.now())
+print("Subtracted date: ", datetime.datetime.now() - datetime.timedelta(days=5))
+
+#ex 2
+
+yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+print("Yesterday:", yesterday.strftime("%x"))
+today = datetime.datetime.now()
+print("Today:", today.strftime("%x"))
+tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+print("Tomorrow:", tomorrow.strftime("%x"))
+
+#ex 3
+
+#datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") also we can use
+print(datetime.datetime.now().isoformat(sep=" ",timespec= "seconds"))
+
+#ex 4
+
+def date_difference_in_seconds(date_str1, date_str2, date_format='%Y-%m-%d %H:%M:%S'):
+    date1 = datetime.strptime(date_str1, date_format)
+    date2 = datetime.strptime(date_str2, date_format)
+    time_difference = abs((date1 - date2).total_seconds())
+    return time_difference
+
+date_str1 = '2022-01-01 12:00:00'
+date_str2 = '2022-02-01 18:30:00'
+
+difference_in_seconds = date_difference_in_seconds(date_str1, date_str2)
+print(f'The difference between the two dates is {difference_in_seconds} seconds.')
---
+++ Practice/lab 4/generators.py
@@ -0,0 +1,50 @@
+
+#ex 1
+def squares(N):
+    for i in range(N): #or range(1, N + 1) если N влючительно
+        yield i**2
+    return N
+
+for square in squares(5):
+    print(square)
+
+#ex 2
+    
+def even_numbers(n):
+    for i in range(0, n + 1):
+        if i % 2 == 0:
+            yield i
+
+n = int(input("Enter a number: "))
+even_nums = list(even_numbers(n))
+print("Even numbers from", n, "to 0:", end=" ")
+print(", ".join(map(str, even_nums)))
+
+#ex 3
+def divisible_by_three_and_four(n):
+    for i in range(0, n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
+for num in divisible_by_three_and_four(40):
+    print(num)
+
+#ex 4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i ** 2
+
+for square in squares(1, 5):
+    print(square)
+
+#ex 5
+def countdown_generator(n):
+    while n >= 0:
+        yield n
+        n -= 1
+
+n = int(input("Enter a value for n: "))
+list_nums = list(countdown_generator(n))
+print("Countdown from", n, "to 0:", end=" ")
+print(", ".join(map(str, list_nums)))
+
