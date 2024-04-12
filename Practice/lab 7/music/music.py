37a3cc2207e35922dd43f29c95bb39f6a7160891
assemaseid <seidkarim601705@gmail.com>
2024-04-12 12:05:07 +0500

    music

--- Practice/lab 7/music/music.py
+++ Practice/lab 7/music/music.py
@@ -1,32 +1,38 @@
 import pygame
+
 pygame.init()
-screen_widht=640
-screen_height=480
-screen = pygame.display.set_mode((screen_height,screen_widht))
-tracks=['lab 7/music/first.mp3', 'lab 7/music/second.mp3', 'lab 7/music/third.mp3']
-current_track=0
-pygame.mixer.music.load(tracks[current_track])
-running= True
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Keyboard Music Player")
+
+songs =['lab 7/music/first.mp3', 'lab 7/music/second.mp3', 'lab 7/music/third.mp3']
+
+
+current_song_index = 0
+pygame.mixer.music.load(songs[current_song_index])
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running=False
+            running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
+            if event.key == pygame.K_SPACE:
                 if pygame.mixer.music.get_busy():
-                    pygame.mixer.music.stop()
+                    pygame.mixer.music.pause()
                 else:
                     pygame.mixer.music.play()
+            elif event.key == pygame.K_RIGHT:
+                current_song_index = (current_song_index + 1) % len(songs)
+                #0+1%3 = 1(play second song)
+                #1+1%3 = 2(play third song)
+                #2+1%3 = 0(play first song again)
+                pygame.mixer.music.load(songs[current_song_index])
+                pygame.mixer.music.play()
             elif event.key == pygame.K_LEFT:
-                current_track=(current_track-1)%len(tracks)
-                pygame.mixer.music.load(tracks[current_track])
-                pygame.mixer.music.play()
-            elif event.key==pygame.K_RIGHT:
-                current_track=(current_track+1)%len(tracks)
-                pygame.mixer.music.load(tracks[current_track])
+                current_song_index = (current_song_index - 1) % len(songs)
+                pygame.mixer.music.load(songs[current_song_index])
                 pygame.mixer.music.play()
 
-
-    screen.fill((255,255,255))
-    pygame.display.flip()
 pygame.quit()
+
