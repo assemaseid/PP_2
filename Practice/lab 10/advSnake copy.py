ca5d8d39940291c418e4ad4aaa43d0ad4d6d33ec
assemaseid <seidkarim601705@gmail.com>
2024-04-29 21:32:41 +0500

    Snake

--- Practice/lab 10/advSnake copy.py
+++ Practice/lab 10/advSnake copy.py
@@ -116,6 +116,7 @@ def level_1_walls():
         wall_y += 64
 
 def level_2_walls(screen):
+    global rock_walls
     rock_walls = pygame.image.load("/Users/assemseidkarim/Desktop/PP_2/Practice/lab 10/rock.png")
     rock_walls_x = 60
     rock_walls_y = screen_height // 2
@@ -141,10 +142,56 @@ def level_2_walls(screen):
         screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
         rock_walls_x += 32
 
-# def level_2_walls(screen):
+def level_3_walls(screen):
+    rock_walls = pygame.image.load("/Users/assemseidkarim/Desktop/PP_2/Practice/lab 10/rock.png")
+# vertical walls
+    rock_walls_x = screen_width // 5
+    rock_walls_y = screen_height // 3
+    while rock_walls_y < screen_width - (screen_width // 2):
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_y += 32
 
+    rock_walls_x = (screen_width // 5) * 2
+    rock_walls_y = screen_height // 3
+    while rock_walls_y < screen_width - (screen_width // 2):
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_y += 32
 
+    rock_walls_x = (screen_width // 5) * 3
+    rock_walls_y = screen_height // 3
+    while rock_walls_y < screen_width - (screen_width // 2):
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_y += 32
+    
+    rock_walls_x = (screen_width // 5) * 4
+    rock_walls_y = screen_height // 3
+    while rock_walls_y < screen_width - (screen_width // 2):
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_y += 32
+# horizomntal
+    rock_walls_x = screen_width // 5 + 32
+    rock_walls_y = screen_height // 3 - 60
+    while rock_walls_x < (screen_width // 5) * 2 - 20:
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_x += 32
 
+    rock_walls_x = screen_width // 5 + 32
+    rock_walls_y = screen_width - (screen_width // 2) + 60
+    while rock_walls_x < (screen_width // 5) * 2 - 20:
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_x += 32
+
+    rock_walls_x = (screen_width // 5) * 3 + 32
+    rock_walls_y = screen_width - (screen_width // 2) + 60
+    while rock_walls_x < (screen_width // 5) * 4 - 20:
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_x += 32
+
+    rock_walls_x = (screen_width // 5) * 3 + 32
+    rock_walls_y = screen_height // 3 - 60
+    while rock_walls_x < (screen_width // 5) * 4 - 20:
+        screen.blit(rock_walls,(rock_walls_x,rock_walls_y))
+        rock_walls_x += 32
 
 def display_score_level(score, level):
     value = font_style.render("Score: " + str(score) + "  Level: " + str(level), True, black)
@@ -161,43 +208,75 @@ def check_food_collision(snake_pos, food_pos, food_pos_2):
 
 
 def update_food_pos(snake_body):
-    global last_food_time
+    global last_food_time, food_width
     food_size = random.randint(10, 30)
     food_width = food_size
     food_pos = [
-        random.randrange(2, (screen_width // snake_block_size)-3) * snake_block_size,
-        random.randrange(4, (screen_height // snake_block_size)-3) * snake_block_size
+        random.randrange(3, (screen_width // snake_block_size)-3) * snake_block_size,
+        random.randrange(4, (screen_height // snake_block_size)-4) * snake_block_size
     ]
     food_pos_2 = [
-        random.randrange(2, (screen_width // snake_block_size)-3) * snake_block_size,
-        random.randrange(4, (screen_height // snake_block_size)-3) * snake_block_size
+        random.randrange(3, (screen_width // snake_block_size)-3) * snake_block_size,
+        random.randrange(4, (screen_height // snake_block_size)-4) * snake_block_size
     ]
-
     for pos in snake_body:
         if [food_pos, food_pos_2] == pos:
             return update_food_pos(snake_body)
+        
+    rock_wall_positions = [
+            (range(60, screen_width // 3), range(screen_height // 2 - 32, screen_height // 2 + 32)),  # Top-left rock wall
+            (range(screen_width // 2 - 32, screen_width // 2 + 32), range(89, screen_height // 3)),   # Top-right rock wall
+            (range(screen_width // 2 - 32, screen_width // 2 + 32), range(screen_height - screen_height // 3, screen_height - 64)),  # Bottom-right rock wall
+            (range(screen_width - screen_width // 3 - 32, screen_width - 50), range(screen_height // 2 - 32, screen_height // 2 + 32))    # Bottom-left rock wall
+            ]
+    
+    for wall_pos in rock_wall_positions:
+        if  food_pos == wall_pos or food_pos_2 == wall_pos:
+            return update_food_pos(snake_body)
+    
 
     last_food_time = pygame.time.get_ticks()
     return food_pos, food_pos_2, food_size
 
 def check_wall_collision(snake_pos, level):
     # Check for collision with the boundaries of the screen
+    global rock_wall_positions
     if level == 1:
         if snake_pos[0] >= screen_width - 64 or snake_pos[0] < 50 or snake_pos[1] >= screen_height - 64 or snake_pos[1] < 64:
             return True
-    if level >= 2:
+    if level == 2:
         if snake_pos[0] >= screen_width - 64 or snake_pos[0] < 50 or snake_pos[1] >= screen_height - 64 or snake_pos[1] < 64:
             return True
         
         rock_wall_positions = [
-            (range(60, screen_width // 3), range(screen_height // 2 - 16, screen_height // 2 + 16)),  # Top-left rock wall
-            (range(screen_width // 2 - 16, screen_width // 2 + 16), range(89, screen_height // 3)),   # Top-right rock wall
-            (range(screen_width // 2 - 16, screen_width // 2 + 16), range(screen_height - screen_height // 3 - 32, screen_height - 64)),  # Bottom-right rock wall
-            (range(screen_width - screen_width // 3 - 32, screen_width - 50), range(screen_height // 2 - 16, screen_height // 2 + 16))    # Bottom-left rock wall
+            (range(60, screen_width // 3), range(screen_height // 2 - 16, screen_height // 2 + 16)), 
+            (range(screen_width // 2 - 16, screen_width // 2 + 16), range(89, screen_height // 3)),  
+            (range(screen_width // 2 - 16, screen_width // 2 + 16), range(screen_height - screen_height // 3, screen_height - 64)),  
+            (range(screen_width - screen_width // 3 - 32, screen_width - 50), range(screen_height // 2 - 16, screen_height // 2 + 16))    
             ]
         for wall_x_range, wall_y_range in rock_wall_positions:
             if snake_pos[0] in wall_x_range and snake_pos[1] in wall_y_range:
                 return True
+    if level >= 3:
+        if snake_pos[0] >= screen_width - 64 or snake_pos[0] < 50 or snake_pos[1] >= screen_height - 64 or snake_pos[1] < 64:
+            return True
+        
+        rock_wall_positions = [
+            (range(screen_width // 5 - 16, screen_width // 5 + 16), range(screen_height // 3,screen_width - (screen_width // 2))),  
+            (range((screen_width // 5) * 2 - 16, (screen_width // 5) * 2 + 16), range(screen_height // 3, screen_width - (screen_width // 2))),
+            (range((screen_width // 5) * 3 - 16, (screen_width // 5) * 3 + 16), range(screen_height // 3, screen_width - (screen_width // 2))),  
+            (range((screen_width // 5) * 4 - 16, (screen_width // 5) * 4 + 16), range(screen_height // 3, screen_width - (screen_width // 2))),  
+
+            (range(screen_width // 5 + 32, (screen_width // 5) * 2 - 20), range(screen_height // 3 - 60 - 16, screen_height // 3 - 60 + 16)),
+            (range(screen_width // 5 + 32, (screen_width // 5) * 2 - 20), range(screen_width - (screen_width // 2) + 60 - 16, screen_width - (screen_width // 2) + 60 + 16)),
+            (range((screen_width // 5) * 3 + 32, (screen_width // 5) * 4 - 20), range(screen_width - (screen_width // 2) + 60 - 16, screen_width - (screen_width // 2) + 60 + 16)),    
+            (range((screen_width // 5) * 3 + 32, (screen_width // 5) * 4 - 20), range(screen_height // 3 - 60 - 16, screen_height // 3 - 60 + 16))    
+            ]
+        
+        for wall_x_range, wall_y_range in rock_wall_positions:
+            if snake_pos[0] in wall_x_range and snake_pos[1] in wall_y_range:
+                return True
+
     
   
 
@@ -258,14 +337,10 @@ while not game_over:
         pygame.mixer.music.play()
         food_pos, food_pos_2, food_width = update_food_pos(snake_body)
         snake_body.insert(0, list(snake_pos))
-        if score % 3 == 0:
+        if score % 5 == 0:
             level += 1
             snake_speed += 2
-            if level == 2:
-                level_2_walls(screen)
                 
-                
-
     else:
         snake_body.insert(0, list(snake_pos))
         snake_body.pop()
@@ -277,7 +352,7 @@ while not game_over:
         game_over = True
         insert_scores_data(username, score, level)        
 
-    screen.fill(white)
+    screen.fill((0,100,0))
     display_snake(snake_body)
     pygame.draw.rect(screen, red, pygame.Rect(food_pos[0], food_pos[1], food_width, food_width))
     pygame.draw.rect(screen, red, pygame.Rect(food_pos_2[0], food_pos_2[1], food_width, food_width))
@@ -286,9 +361,12 @@ while not game_over:
 
     if level == 1:
         level_1_walls()
-    elif level >= 2 :
+    elif level == 2 :
         level_1_walls()
         level_2_walls(screen)
+    elif level >= 3:
+        level_1_walls()
+        level_3_walls(screen)
     
 
     pygame.display.update()
